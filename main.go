a2864056e83824488937022834232d157d69d66a
cryonayes <cryonayes@gmail.com>
2021-07-26 16:44:09 +0300

    Project structure seperation
    It turns out nextjs exporting can't use server side rendering. So after now GoLang will be used as auth & file handling service on port 21942.

--- main.go
+++ main.go
@@ -1,37 +1,14 @@
 package main
 
 import (
-	"embed"
 	"fmt"
 	"github.com/cryonayes/GoShare/database"
-	"io/fs"
-	"log"
-	"os"
-
-	"github.com/cryonayes/GoShare/api/file"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"os"
 )
 
-//go:embed nextjs/dist
-//go:embed nextjs/dist/_next
-//go:embed nextjs/dist/_next/static/chunks/pages/*.js
-//go:embed nextjs/dist/_next/static/*/*.js
-var nextFS embed.FS
-
 func main() {
-
-	distFS, err := fs.Sub(nextFS, "nextjs/dist")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if _, err := os.Stat(file.UploadDir); os.IsNotExist(err) {
-		err := os.Mkdir(file.UploadDir, os.ModeType)
-		if err != nil {
-			_, _ = fmt.Fprintln(os.Stderr, "Cannot create uploads directory!")
-		}
-	}
 
 	app := fiber.New(fiber.Config{})
 
@@ -40,11 +17,11 @@ func main() {
 		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
 
-	Setup(app, distFS)
+	Setup(app)
 
 	database.Connect()
 
-	err = app.Listen(":8080")
+	err := app.Listen(":21942")
 	if err != nil {
 		_, _ = fmt.Fprintln(os.Stderr, "Cannot initialize server!")
 		return
--- routes.go
+++ routes.go
@@ -1,24 +1,13 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2/middleware/filesystem"
-	"io/fs"
-	"net/http"
-
 	authApi "github.com/cryonayes/GoShare/api"
 	fileApi "github.com/cryonayes/GoShare/api/file"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
-func Setup(app *fiber.App, fs fs.FS) {
-
-	app.Use("/", filesystem.New(filesystem.Config{
-		Root:  http.FS(fs),
-		Index: "/index.html",
-		NotFoundFile: "/404.html",
-	}))
-
+func Setup(app *fiber.App) {
 	app.Use(cors.New(cors.ConfigDefault))
 	app.Post("/api/login", authApi.Login)
 	app.Post("/api/register", authApi.Register)
